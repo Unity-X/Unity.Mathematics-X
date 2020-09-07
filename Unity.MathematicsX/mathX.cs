5ca6d93c9f4a7c8ca3d427b04f2a98c313a6baf6
Frédéric Bessette <msfredb7@gmail.com>
2020-09-07 13:04:15 -0400

    sign methods

--- Unity.MathematicsX/mathX.cs
+++ Unity.MathematicsX/mathX.cs
@@ -14,6 +14,30 @@ namespace Unity.MathematicsX
         {
             return math.atan2(v.y, v.x);
         }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int sign(int v)
+        {
+            return Math.Sign(v);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int2 sign(int2 v)
+        {
+            return math.int2(sign(v.x), sign(v.y));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int3 sign(int3 v)
+        {
+            return math.int3(sign(v.x), sign(v.y), sign(v.z));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int4 sign(int4 v)
+        {
+            return math.int4(sign(v.x), sign(v.y), sign(v.z), sign(v.w));
+        }
     }
 #pragma warning restore IDE1006 // Naming Styles
 }
